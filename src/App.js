844aaa963c7408d27cfae8fd0dc348e82488dfd7
sreeram <kosireddisreeram@gmail.com>
2021-09-29 16:30:34 +0530

    added sass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"; 
+import Board from "./components/Board";
+import  "./styles/root.scss";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <span  style={{ marginBottom :'40px',marginTop:'10px'}}>TIC TAC TOE</span> 
+         <Board/>
     </div>
   );
 }
